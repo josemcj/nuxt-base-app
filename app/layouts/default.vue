e614bdb1d7bc28f721c40b1880aa0b4cb4e0dd12
Jose Contreras <desarrollo.11@nexen-elog.com>
2026-08-06 20:51:51 -0600

    feat: dividir header en componente

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -29,47 +29,7 @@ function toggleMenu() {
     </div>
 
     <div id="layout-wrapper">
-      <header id="page-topbar">
-        <div class="navbar-header">
-          <div class="d-flex">
-            <div class="navbar-brand-box">
-              <NuxtLink to="/" class="logo logo-dark">
-                <span class="logo-sm">
-                  <img src="~/assets/images/logo.svg" alt="Skote theme" height="22" />
-                </span>
-
-                <span class="logo-lg">
-                  <img src="~/assets/images/logo.svg" alt="Skote theme" height="34" />
-                </span>
-              </NuxtLink>
-
-              <NuxtLink to="/" class="logo logo-light">
-                <span class="logo-sm">
-                  <img src="~/assets/images/logo-light.svg" alt="Skote theme" height="22" />
-                </span>
-
-                <span class="logo-lg">
-                  <img src="~/assets/images/logo-light.svg" alt="Skote theme" height="34" />
-                </span>
-              </NuxtLink>
-            </div>
-
-            <button
-              type="button"
-              class="btn btn-sm px-3 font-size-16 d-lg-none header-item"
-              aria-label="Mostrar u ocultar navegación"
-              :aria-expanded="menuOpen"
-              @click="toggleMenu">
-              <i class="fa fa-fw fa-bars" />
-            </button>
-          </div>
-
-          <div class="d-flex align-items-center">
-            <span class="d-none d-sm-inline-block">Skote theme</span>
-          </div>
-        </div>
-      </header>
-
+      <AppHeader :menu-open="menuOpen" @toggle-menu="toggleMenu" />
       <AppHorizontalNav :open="menuOpen" @close="menuOpen = false" />
 
       <div class="main-content">
