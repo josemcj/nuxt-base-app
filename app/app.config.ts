f56686bacb1edca7e2cff0517ea21eb873164627
Jose Contreras <mexidios.mx@gmail.com>
2026-08-06 21:42:32 -0600

    config: titulo genera del sitio

---
+++ app/app.config.ts
@@ -0,0 +1,4 @@
+export default defineAppConfig({
+  title: 'Skote theme',
+  description: 'Skote theme App.',
+});
--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+const appConfig = useAppConfig();
+
+useHead({
+  titleTemplate: (pageTitle) => {
+    if (!pageTitle) {
+      return appConfig.title;
+    }
+
+    return `${pageTitle} | ${appConfig.title}`;
+  },
+});
+
+useSeoMeta({
+  description: appConfig.description,
+});
+</script>
+
 <template>
   <BApp>
     <NuxtRouteAnnouncer />
