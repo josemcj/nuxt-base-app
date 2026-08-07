d28422ae6465931cbda1d8f45b6b6fa079d3a2a6
Jose Contreras <mexidios.mx@gmail.com>
2026-08-06 21:31:17 -0600

    config: ajuste de layout

--- app/stores/layout.ts
+++ app/stores/layout.ts
@@ -2,7 +2,7 @@ import type { LayoutState } from '~/types/layouts';
 
 export const useLayoutStore = defineStore('layout', {
   state: (): LayoutState => ({
-    layoutType: 'vertical',
+    layoutType: 'horizontal',
     layoutWidth: 'fluid',
     leftSidebarType: 'dark',
     topbar: 'dark',
